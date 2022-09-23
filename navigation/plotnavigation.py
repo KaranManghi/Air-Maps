b3d4c39fa2ac36eb27dc0ff54f1ccccc27915584
salonishah331 <salonishah331@gatech.edu>
2022-09-22 17:12:37 -0700

    added visualization with a map

--- navigation/plotnavigation.py
+++ navigation/plotnavigation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 # import matplotlib.pyplot.figure as figure
 import numpy as np
 
-coordinates = [(0, 0), (1, 1), (1, 2), (2, 3), (3, 4), (3, 5)]
+coordinates = [(0, 0), (1000, 1000), (1000, 2000), (2000, 3000), (3000, 4000), (3000, 5000)]
 xcoordinates =[]
 ycoordinates = []
 
@@ -13,8 +13,8 @@ for coord in coordinates:
 
 
 mapplot = plt.plot(xcoordinates,ycoordinates)
-# map = plt.imread('map.jpg')
-# mapplot.figure.figimage(map, 40, 40, alpha=.15, zorder=1)
+map = plt.imread('map.jpg')
+plt.imshow(map)
 plt.show()
 
 
